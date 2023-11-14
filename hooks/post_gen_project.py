8ac525ff8c51b25915b1140567315280c271a99d
Mauricio Franco <maufranzar@gmail.com>
2023-11-14 16:46:08 -0500

    Mod:hooks

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,13 @@
+import subprocess
+
+MESSAGE_COLOR = "\x1b[34m"
+RESET_ALL = "\x1b[0m"
+
+print(f"{MESSAGE_COLOR}Almost done!")
+print(f"Initializing a git repository...{RESET_ALL}")
+
+subprocess.call(['git', 'init'])
+subprocess.call(['git', 'status'])
+subprocess.call(['git', 'lg'])
+
+print(f"{MESSAGE_COLOR} :v {RESET_ALL}")
