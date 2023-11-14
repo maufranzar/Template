e8aaddb014c6b42f8ababc854b148b17aaa8c041
Mauricio Franco <maufranzar@gmail.com>
2023-11-14 17:26:37 -0500

    Auto:Git-pull

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,11 +3,7 @@ import subprocess
 MESSAGE_COLOR = "\x1b[34m"
 RESET_ALL = "\x1b[0m"
 
-print(f"{MESSAGE_COLOR}Almost done!")
-print(f"Initializing a git repository...{RESET_ALL}")
-
-subprocess.call(['git', 'init'])
 subprocess.call(['git', 'status'])
-subprocess.call(['git', 'lg'])
-
+print(f"{MESSAGE_COLOR}Almost done!")
+print("Actualiazndo Repositorio desde GitHub :D")
 print(f"{MESSAGE_COLOR} powered by {RESET_ALL}")
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import subprocess
 
 project_slug = "{{ cookiecutter.project_slug }}"
 
@@ -8,6 +9,10 @@ MESSAGE_COLOR = "\x1b[34m"
 RESET_ALL = "\x1b[0m"
 
 print(f'{MESSAGE_COLOR}Iniciando Entorno...')
+
+subprocess.call(['git', 'init'])
+subprocess.call(['git', 'status'])
+subprocess.call(['git', 'pull', 'origin', 'Data'])
 
 if project_slug.startswith("x"):
     print(f'{ERROR_COLOR}ERROR: {project_slug=} is not a valid name for this template.{RESET_ALL}')
