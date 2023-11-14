3c136b6289144a819d9e95c7605f73399e9688f3
Mauricio Franco <maufranzar@gmail.com>
2023-11-14 17:55:35 -0500

    Mod:hooks

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,7 +3,7 @@ import subprocess
 MESSAGE_COLOR = "\x1b[34m"
 RESET_ALL = "\x1b[0m"
 
-subprocess.call(['git', 'status'])
+subprocess.call(['tree'])
 
 print(f"{MESSAGE_COLOR}Almost done!")
 print("Actualiazndo Repositorio desde GitHub :D")
