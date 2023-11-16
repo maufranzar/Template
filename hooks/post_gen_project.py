e4f5ca256fcf2d8553210b3d0b06e5b09b43972e
Mauricio Franco <maufranzar@gmail.com>
2023-11-16 11:42:59 -0500

    Mod:Hooks

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,7 +1,18 @@
 import subprocess
 
+ERROR_COLOR = "\x1b[31m"
 MESSAGE_COLOR = "\x1b[34m"
 RESET_ALL = "\x1b[0m"
 
+print(f'{ERROR_COLOR} Template created!{RESET_ALL}')
 subprocess.call(['tree'])
-print(f"{MESSAGE_COLOR}try: conda env create --file environment.yml {RESET_ALL}")
+print(f"{MESSAGE_COLOR}Initializing Git...{RESET_ALL}")
+subprocess.call(['git', 'init'])
+subprocess.call(['git', 'add', '.gitignore'])
+subprocess.call(['git', 'add', '.'])
+print(f"{MESSAGE_COLOR}First Commit{RESET_ALL}")
+subprocess.call(['git', 'commit', '-m', '"First Commit!"'])
+print(f'{ERROR_COLOR} Try to configure Git!{RESET_ALL}')
+
+print(f"{MESSAGE_COLOR}Now, create your environment:
+      conda env create --file environment.yml {RESET_ALL}")
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,10 +1,10 @@
 import os
-import sys
 
 project_slug = "{{ cookiecutter.project_slug }}"
+
 ERROR_COLOR = "\x1b[31m"
 MESSAGE_COLOR = "\x1b[34m"
 RESET_ALL = "\x1b[0m"
 
-print(f"{MESSAGE_COLOR}Let's do it! You're going to create something awesome!")
-print(f"Creating project at { os.getcwd() }{RESET_ALL}")
+print(f"{MESSAGE_COLOR}Creating a new project...")
+print(f"Proyect path: { os.getcwd() }{RESET_ALL}")
